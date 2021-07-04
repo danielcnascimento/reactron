db631bc7d4eff34c8a4a70c4f5e50c2c2cb68fc8
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-07-04 15:51:37 -0300

    adding homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -9,7 +9,19 @@ export default function Home() {
       <title>HomePage | Reactron</title>
     </Head>
     
-    <h1 className={styles.title} >Hello World!</h1>
+    <main className={styles.contentContainer} >
+      <section className={styles.hero} >
+        <span>👋 Hey, Welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications <br /> 
+          <span>for $9.90 month</span>
+        </p> 
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl Coding" />
+
+    </main>
     </>
   )
 }
