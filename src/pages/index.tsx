fa00b8434e7e0ee39efb080218967f06a1bb7133
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-07-06 17:54:29 -0300

    adding static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe';
 
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1J9bcyJvCQ7ZyjIGWxvfrOcn')
 
   const product = {
@@ -55,7 +55,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
  
