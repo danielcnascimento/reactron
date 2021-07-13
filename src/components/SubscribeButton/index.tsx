c9b904bd926db74690e2685fa1cdef1139a19e20
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-07-13 12:33:03 -0300

    added function to signin if not logged.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { session, signIn } from "next-auth/client";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,10 +6,19 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const handleSubscribe = () => {
+    if (!session) {
+      signIn('github')// função next-auth, que redireciona para tela de sign in usando providefer Github.
+      
+      return;
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe Now
     </button>
