190aa3eaf8e31d17a21ba5a168da68f82ce18c1d
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-12-30 20:48:17 -0300

    adding fauna query on signIn callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,7 @@
-import { query as q } from 'faunadb';
-
 import NextAuth from 'next-auth'
-import { fauna } from '../../../services/fauna'
-
 import GithubProvider from 'next-auth/providers/github'
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -17,39 +15,38 @@ export default NextAuth({
       },
     }),
   ],
-  // jwt: {
-  //   secret: process.env.JWT_SIGNING_PRIVATE_KEY,
-  // },
-  // callbacks : {
-  //   async signIn({user, account, profile}){
-  //     const { email } = user;
-  //     console.log(user)
+  jwt: { // WARN: chave gerada de forma aleatória, chave sem valor de negócio.
+    secret: process.env.JWT_SIGNING_PRIVATE_KEY,
+  },
+  callbacks : {// MEANING: fica escutando os métodos de auto do next.js, e realiza ações ao serem chamados.
+    async signIn({ user }){ // MEANING: chamada assincrona que faz login caso retorno seja true.
+      const { email } = user;
 
-  //     try {
-  //       await fauna.query(
-  //         q.If(q.Not( q.Exists(q.Match(
-  //                 q.Index('user_by_email'),
-  //                 q.Casefold(user.email),
-  //               )
-  //             )
-  //           ),
-  //           //then
-  //           q.Create(q.Collection('users'),
-  //             { data: { email } },
-  //           ),
-  //           //else
-  //           q.Get(q.Match(
-  //               q.Index('user_by_email'),
-  //               q.Casefold(user.email),
-  //             )
-  //           )
-  //         )
-  //       )
-        
-  //       return true
-  //     } catch {
-  //       return false
-  //     }
-  //   }
-  // }
+      try {
+        await fauna.query(
+          q.If(q.Not(q.Exists(q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email),
+                )
+              )
+            ),
+            //then
+            q.Create(q.Collection('users'),
+              { data: { email } },
+            ),
+            //else
+            q.Get(q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email),
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
